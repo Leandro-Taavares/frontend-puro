f66f5a714100e3b38d439144e88c47bec5efb84e
Leandro Tavares <leandrotavares.2304@gmail.com>
2024-07-11 12:35:59 -0300

    feat() detalhe finalizado

--- pagina-estoque-detalhe/script.js
+++ pagina-estoque-detalhe/script.js
@@ -25,7 +25,7 @@ ajax.onreadystatechange = () => {
         corP.innerHTML = car.cor;
 
         var trocaP = document.querySelector(".Troca p:nth-child(2)");
-        trocaP.innerHTML = car.troca ? "Sim" : "Não";;
+        trocaP.innerHTML = car.troca ? "Sim" : "Não";
 
         var donoP = document.querySelector(".Dono p:nth-child(2)");
         donoP.innerHTML = car.unico_dono ? "Sim" : "Não";
@@ -35,5 +35,44 @@ ajax.onreadystatechange = () => {
 
         var infotextoP = document.querySelector(".informações-texto"); 
         infotextoP.innerHTML = car.informacoes;
+
+        var opcionais = car.opcionais;
+        var colunas = document.querySelectorAll(".itens-opcionais > div");
+        var colIndex = 0;
+
+        opcionais.forEach(opcional => {
+            var opcionalDiv = document.createElement("div");
+            opcionalDiv.classList.add("alinha-lista");
+            var img = document.createElement("img");
+            img.src = "imagens/Check-opcionais.png";
+            img.alt = "check";
+            var p = document.createElement("p");
+            p.classList.add("texto-opcionais");
+            p.innerHTML = opcional;
+
+            opcionalDiv.appendChild(img);
+            opcionalDiv.appendChild(p);
+
+            colunas[colIndex].appendChild(opcionalDiv);
+            colIndex = (colIndex + 1) % colunas.length; // Mudar para a próxima coluna
+        });
     }
-}
+
+    var swiperWrapper = document.querySelector('.swiper-wrapper');
+
+        // Limpa qualquer conteúdo existente dentro do swiperWrapper
+        swiperWrapper.innerHTML = '';
+
+        // Adiciona cada imagem do JSON como um slide ao swiperWrapper
+        car.imagem.forEach(imageUrl => {
+            var swiperSlide = document.createElement('div');
+            swiperSlide.classList.add('swiper-slide');
+            
+            var img = document.createElement('img');
+            img.src = imageUrl;
+            img.alt = car.nome; // Pode ser alterado para o que desejar
+
+            swiperSlide.appendChild(img);
+            swiperWrapper.appendChild(swiperSlide);
+        });
+};
